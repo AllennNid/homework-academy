0fe5c695bba4aa61bd78b6d5f2c624a278a517d8
AllennNid <122411800+AllennNid@users.noreply.github.com>
2023-01-25 02:10:04 +0100

    Add files via upload

---
+++ funk3.js
@@ -0,0 +1,10 @@
+Intl.DateTimeFormat('ua').format(1674068932372) // "16.09.2020"                                                                                                                                                                                   sortDates(["2022-01-16T19:08:26.142Z", 1674068932372, '1/19/2021, 9:09:55 PM'])                                                                                                                                                                              
+function sortEggsInNest(a, b) {
+if (a > b) {
+return 1;
+} else if (b > a) {
+return -1;
+} else {
+return 0;
+}
+}
